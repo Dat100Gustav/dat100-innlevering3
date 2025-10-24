053c5b5b7874e2b4237cd06ef69286959c9e35b8
EAS77896 <eiska1991@hvl.no>
2025-10-24 18:19:34 +0200

    ferdig med innlevering 3

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -4,12 +4,32 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
+import no.hvl.dat100.oppgave1.Innlegg;
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+        boolean harSkrevet = false;
+        PrintWriter writer = null;
+
+        try {
+            writer = new PrintWriter(mappe + filnavn);
+            writer.println(samling.toString());
+            harSkrevet = true;
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Kunne ikke skrive til filen: " + e.getMessage());
+        }
+        finally {
+            if (writer != null){
+                writer.close();
+            }
+        }
+
+        return harSkrevet;
+
 	}
 }
